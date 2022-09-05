4cda53c5343c7c69bed03f90acd43eb64962c353
branyoe <bhernandez1@ucol.mx>
2022-09-04 21:02:20 -0500

    Refactor: Changes in main.js

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
-const coladeraEratostenes = (limite = 10) => {
+const coladeraEratostenes = (limite = 1000) => {
   //paso 1
   const lista = [];
+  lista.length = limite;
+  lista.fill(1);
   
-  for (let i = 0; i < limite; i++) {
-    lista[i] = 1;
-  }
+  // for (let i = 0; i < limite; i++) {
+  //   lista[i] = 1;
+  // }
+
   
   //paso 2
   for (let i = 2; i < lista.length; i++) {
-    const element = lista[i];
-    if (element === 1) {
+    const elemento = lista[i];
+    if (elemento === 1) {
       for (let j = i + 1; j < lista.length; j++) {
         if (j % i === 0) {
           lista[j] = 0;
@@ -22,4 +25,4 @@ const coladeraEratostenes = (limite = 10) => {
 }
 
 
-console.table(coladeraEratostenes(1000));
+console.table(coladeraEratostenes());
